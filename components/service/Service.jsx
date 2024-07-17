4140d8de173a1216493d2a69cddc438907be9d65
Flames <isaacomenuche@gmail.com>
2024-07-17 13:49:14 +0100

    service section

--- components/service/Service.jsx
+++ components/service/Service.jsx
@@ -1,9 +1,10 @@
 import WorkProcess from "./WorkProcess";
+import ServiceList from "./ServiceList";
 
 export default function Service() {
   return (
     <div>
-      <div className="w-[90%] max-w-[1440px] mx-auto flex md:flex-row flex-col justify-between items-center gap-6 py-10">
+      <div className="w-[90%] max-w-[1440px] mx-auto flex md:flex-row flex-col justify-between items-center gap-10 pt-10 pb-20">
         <h1 className="lg:text-7xl text-5xl font-medium">SERVICES</h1>
         <p className="lg:text-lg text-base font-normal text-justify leading-5 max-w-md">
           Lorem ipsum dolor sit amet consectetur. Elementum quis aenean ultrices
@@ -12,6 +13,7 @@ export default function Service() {
         </p>
       </div>
       <WorkProcess />
+      <ServiceList />
     </div>
   );
 }
