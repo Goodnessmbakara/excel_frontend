4e52e4af7f35ba7d960dd3e938d8c9408501bb46
Flames <isaacomenuche@gmail.com>
2024-07-17 07:22:20 +0100

    responsive videography section

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -11,8 +11,7 @@ export default function Home() {
       <Showcase />
       <AboutSection />
       <Collection />
-      {/*
-      <Videography /> */}
+      <Videography />
     </div>
   );
 }
--- components/home/Videography.jsx
+++ components/home/Videography.jsx
@@ -1,10 +1,14 @@
+import { FaRegCirclePlay } from "react-icons/fa6";
+
 export default function Videography() {
   return (
-    <div className="pb-20 w-[90%] max-w-[1440px] mx-auto">
+    <div className="py-10 w-[90%] max-w-[1440px] mx-auto">
       <h2 className="text-3xl font-medium text-center pb-8">
         We deliver quality video coverage for various events
       </h2>
-      <div className="bg-black h-[500px]"></div>
+      <div className="bg-gray-900 max-w-xl mx-auto h-96 flex items-center justify-center">
+        <FaRegCirclePlay className="text-white text-6xl" />
+      </div>
     </div>
   );
 }
