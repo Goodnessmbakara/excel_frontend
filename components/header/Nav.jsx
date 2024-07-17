6442db2906e35cfcd0698fe4012b7bbeaa5d2ae1
Flames <isaacomenuche@gmail.com>
2024-07-17 14:03:13 +0100

    update header positon

--- components/header/Nav.jsx
+++ components/header/Nav.jsx
@@ -1,13 +1,39 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Nav({ className }) {
+  const pathname = usePathname();
+  console.log(pathname);
+
   return (
     <div
       className={`flex gap-3 justify-between text-base font-medium uppercase ${className}`}
     >
-      <Link href={"/service"}>Services</Link>
-      <Link href={"/gallery"}>GALLERY</Link>
-      <Link href={"/about"}>About us</Link>
+      <Link
+        href={"/service"}
+        className={`py-2 ${
+          pathname === "/service" ? "border-b-[1px] border-black" : ""
+        }`}
+      >
+        Services
+      </Link>
+      <Link
+        href={"/gallery"}
+        className={`py-2 ${
+          pathname === "/gallery" ? "border-b-[1px] border-black" : ""
+        }`}
+      >
+        GALLERY
+      </Link>
+      <Link
+        href={"/about"}
+        className={`py-2 ${
+          pathname === "/about" ? "border-b-[1px] border-black" : ""
+        }`}
+      >
+        About us
+      </Link>
     </div>
   );
 }
