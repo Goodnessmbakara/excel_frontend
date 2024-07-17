f5b01f29d52f1c1fc4f4f9a323b6773eef732ff7
Flames <isaacomenuche@gmail.com>
2024-07-17 19:13:13 +0100

    Gallery 👀

---
+++ components/gallery/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <div>
+      <div className="w-[90%] max-w-[1440px] mx-auto flex md:flex-row flex-col justify-between items-center gap-10 pt-10 pb-20">
+        <h1 className="lg:text-7xl text-5xl font-medium">GALLERY</h1>
+        <p className="lg:text-lg text-base font-normal text-justify leading-5 max-w-md">
+          Lorem ipsum dolor sit amet consectetur. Elementum quis aenean ultrices
+          suspendisse justo nunc nullam. Quam ultrices viverra dui quam pretium
+          malesuada.
+        </p>
+      </div>
+    </div>
+  );
+}
