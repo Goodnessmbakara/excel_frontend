9303113b716e0713a804eb3e9ff0bccffc76c908
Flames <isaacomenuche@gmail.com>
2024-07-17 09:43:27 +0100

    service header

---
+++ app/service/page.js
@@ -0,0 +1,9 @@
+import Service from "../../components/service/Service";
+
+export default function page() {
+  return (
+    <div className="min-h-screen">
+      <Service />
+    </div>
+  );
+}
---
+++ components/service/Service.jsx
@@ -0,0 +1,14 @@
+export default function Service() {
+  return (
+    <div>
+      <div className="w-[90%] max-w-[1440px] mx-auto flex md:flex-row flex-col justify-between items-center gap-6 py-10">
+        <h1 className="lg:text-7xl text-5xl font-medium">SERVICES</h1>
+        <p className="lg:text-lg text-base font-normal text-justify leading-5 max-w-md">
+          Lorem ipsum dolor sit amet consectetur. Elementum quis aenean ultrices
+          suspendisse justo nunc nullam. Quam ultrices viverra dui quam pretium
+          malesuada.
+        </p>
+      </div>
+    </div>
+  );
+}
