4544b5d886445c1d2754da590fefc60135155d50
Flames <isaacomenuche@gmail.com>
2024-07-18 08:05:32 +0100

    contact page added

--- components/contact/Contact.jsx
+++ components/contact/Contact.jsx
@@ -1,6 +1,17 @@
+import Link from "next/link";
+import ContactForm from "./ContactForm";
+import { FaPhoneAlt } from "react-icons/fa";
+import {
+  FaLocationDot,
+  FaSquareInstagram,
+  FaFacebook,
+  FaPinterest,
+} from "react-icons/fa6";
+import { IoMail } from "react-icons/io5";
+
 export default function Contact() {
   return (
-    <div>
+    <div className="pb-20">
       <div className="w-[90%] max-w-[1440px] mx-auto flex md:flex-row flex-col justify-between items-center gap-10 pt-10 pb-20">
         <h1 className="lg:text-7xl text-5xl font-medium text-nowrap">
           CONTACT US
@@ -11,7 +22,35 @@ export default function Contact() {
           malesuada.
         </p>
       </div>
-      <div></div>
+      <div className="w-[90%] max-w-[1440px] mx-auto flex md:flex-row flex-col-reverse justify-between items-center gap-20">
+        <div className="flex flex-col gap-10 min-w-[360px]">
+          <div className="flex gap-3 items-center">
+            <FaPhoneAlt className="size-8" />
+            <span>+234595690500</span>
+          </div>
+          <div className="flex gap-3 items-center">
+            <FaLocationDot className="size-8" />
+            <span>
+              31 itiam street off NEPA Line, Uyo Itam, Akwa Ibom, Nigeria 520261
+            </span>
+          </div>
+          <div className="flex gap-6 items-center">
+            <Link href={""}>
+              <FaSquareInstagram className="size-8" />
+            </Link>
+            <Link href={""}>
+              <FaFacebook className="size-8" />
+            </Link>
+            <Link href={""}>
+              <IoMail className="size-8" />
+            </Link>
+            <Link href={""}>
+              <FaPinterest className="size-8" />
+            </Link>
+          </div>
+        </div>
+        <ContactForm />
+      </div>
     </div>
   );
 }
--- components/contact/ContactForm.jsx
+++ components/contact/ContactForm.jsx
@@ -1,3 +1,52 @@
 export default function ContactForm() {
-  return <div>ContactForm</div>;
+  return (
+    <form className="w-full max-w-md flex flex-col gap-10">
+      <div className="">
+        <label htmlFor="name" className="font-normal text-xl">
+          Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          className="w-full h-8 border-b-[1px] border-b-black outline-none"
+        />
+      </div>
+      <div>
+        <label htmlFor="email" className="font-normal text-xl">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="w-full h-8 border-b-[1px] border-b-black outline-none"
+        />
+      </div>
+      <div>
+        <label htmlFor="phone" className="font-normal text-xl">
+          Phone number
+        </label>
+        <input
+          type="tel"
+          id="phone"
+          className="w-full h-8 border-b-[1px] border-b-black outline-none"
+        />
+      </div>
+      <div>
+        <label htmlFor="message" className="font-normal text-xl">
+          Message
+        </label>
+        <textarea
+          id="message"
+          rows={5}
+          className="w-full h-8 border-b-[1px] border-b-black outline-none"
+        />
+      </div>
+      <button
+        type="button"
+        className="bg-black hover:opacity-50 text-white rounded-full max-w-64 py-6 mt-6"
+      >
+        SEND MESSAGE
+      </button>
+    </form>
+  );
 }
