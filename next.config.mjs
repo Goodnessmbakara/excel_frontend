180ef97239ec044da0327cc586bd398df32bfc0e
Flames <isaacomenuche@gmail.com>
2024-07-30 02:29:58 +0100

    correct next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    loader: "custom",
-    loaderFile: "./app/gallery/loading.js",
     remotePatterns: [
       {
         protocol: "https",
