985d7edd1f8ddabf5bc9b8aa9cefe0341222a075
Flames <isaacomenuche@gmail.com>
2024-07-31 13:11:38 +0100

    strange

--- components/about/Team.jsx
+++ components/about/Team.jsx
@@ -1,58 +1,65 @@
+"use client";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 
 export default function Team() {
+  const [teamMembers, setTeamMembers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchTeamMembers() {
+      try {
+        const response = await fetch(
+          "https://app-07b991a0-e1c2-444c-930e-6b13cf0600d2.cleverapps.io/api/our-team/"
+        );
+        const data = await response.json();
+        setTeamMembers(data);
+      } catch (error) {
+        console.error("Error fetching team members:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchTeamMembers();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-[500px]">
+        <div className="lds-ring">
+          <div></div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="w-[80%] mx-auto max-w-[1440px] py-10">
       <h2 className="text-center text-4xl font-medium pb-8">OUR TEAM</h2>
-      <div className="flex md:flex-row flex-col justify-between items-center gap-8">
-        <div className="flex flex-col lg:gap-4 gap-2">
-          <Image
-            src={"/images/team/1.png"}
-            alt="team 1"
-            width={320}
-            height={392}
-          />
-          <div>
-            <p className="xl:text-2xl lg:text-xl text-base font-medium">
-              CAMERON WILLIAMSON
-            </p>
-            <p className="text-[#0000007F] xl:text-xl lg:text-lg text-sm font-normal">
-              CEO
-            </p>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 justify-between">
+        {teamMembers.map((member, index) => (
+          <div
+            key={index}
+            className="flex flex-col lg:gap-4 gap-2 justify-between items-center text-center"
+          >
+            <Image
+              src={member.photo}
+              alt={`${member.name} photo`}
+              width={320}
+              height={392}
+              className="w-full h-auto object-cover"
+            />
+            <div>
+              <p className="xl:text-2xl lg:text-xl text-base font-medium">
+                {member.name.toUpperCase()}
+              </p>
+              <p className="text-[#0000007F] xl:text-xl lg:text-lg text-sm font-normal">
+                {member.title}
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col lg:gap-4 gap-2">
-          <Image
-            src={"/images/team/2.png"}
-            alt="team 1"
-            width={320}
-            height={392}
-          />
-          <div>
-            <p className="xl:text-2xl lg:text-xl text-base font-medium">
-              CAMERON WILLIAMSON
-            </p>
-            <p className="text-[#0000007F] xl:text-xl lg:text-lg text-sm font-normal">
-              CEO
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-col lg:gap-4 gap-2">
-          <Image
-            src={"/images/team/3.png"}
-            alt="team 1"
-            width={320}
-            height={392}
-          />
-          <div>
-            <p className="xl:text-2xl lg:text-xl text-base font-medium">
-              CAMERON WILLIAMSON
-            </p>
-            <p className="text-[#0000007F] xl:text-xl lg:text-lg text-sm font-normal">
-              CEO
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
