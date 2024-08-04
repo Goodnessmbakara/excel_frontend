0ea88f76200d9a38245ad2baf27683652cfeedc6
Flames <isaacomenuche@gmail.com>
2024-08-04 20:25:14 +0100

    hmm

--- components/fetchCollection.js
+++ components/fetchCollection.js
@@ -1,7 +1,10 @@
 export default async function fetchCollection() {
   try {
-    const response = await fetch("/collection/collection.json");
+    const response = await fetch(
+      "https://app-07b991a0-e1c2-444c-930e-6b13cf0600d2.cleverapps.io/api/gallery/"
+    );
     const data = await response.json();
+    console.log(data);
     return data;
   } catch (error) {
     console.error("Error fetching images:", error);
